d0c1ea3fa4ee5091740c329c12b5ad47eb5ed2a5
mihai-gabriel1 <mr.eu21@gmail.com>
2022-07-29 20:39:11 +0300

    Some typo edit has been done.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const ftoc = function (celsius) {
+const ctof = function (celsius) {
   let cTemp = celsius;
   let cToFahr = Math.round((cTemp * 9) / 5 + 32) / 10;
   let message =
     cTemp +
     " celsius grades" +
-    " converted into Fahrenheit grades would be equal to " +
+    " converted into Fahrenheit would be equal to " +
     cToFahr +
     " grades.";
   console.log(message);
 };
-ftoc(20);
+ctof(20);
 
-const ctof = function (fahrenheit) {
+const ftoc = function (fahrenheit) {
   let fTemp = fahrenheit;
   let fToCels = Math.round(((fTemp - 32) * 5) / 9) / 10;
   let message =
     fTemp +
-    " Fahrenheit grades converted into Celsius grades would be equal to " +
+    " Fahrenheit grades converted into Celsius would be equal to " +
     fToCels +
     " grades.";
   console.log(message);
 };
-ctof(35.2);
+ftoc(35);
